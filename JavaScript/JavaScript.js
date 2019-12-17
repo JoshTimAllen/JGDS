f697b9f11da17724f07e4b0956f5465f37dccce7
JoshTimAllen <40319513+JoshTimAllen@users.noreply.github.com>
2019-12-16 19:05:44 -0500

    Add files via upload

---
+++ JavaScript/JavaScript.js
@@ -0,0 +1,84 @@
+
+var showingMenu = false;
+var menu = document.getElementById("ResponsiveMenu");
+console.log(window.innerWidth);
+console.log(window.outerWidth);
+function ShowMenu(){
+
+  if(showingMenu == false){
+    showingMenu = true;
+    menu.style.display="flex";
+  }
+  else {
+    showingMenu = false;
+    menu.style.display="none";
+  }
+}
+
+function mediaQuery(x){
+  if (x.matches) { 
+
+    }
+    else {
+    showingMenu = false;
+    menu.style.display="none";
+    }
+}
+
+var x = window.matchMedia("(max-width: 900px)");
+mediaQuery(x);
+x.addListener(mediaQuery);
+
+var body = document.getElementById("body");
+  
+  var BgVideo = document.getElementById("BgVideo");
+  
+  var VidContainer = document.getElementById("VidContainer");
+
+  var initTransparency = 0.5;
+function scrollfunction(){
+
+  var top = document.getElementById("top");
+
+  
+  
+  var derivedTransparency = 0.0;
+  
+  var derivedMovement = 0.0;
+
+  //derivedMovement = (window.pageYOffset/(body.offsetHeight));
+
+    //sec1.style.bottom = `${((window.pageYOffset-body.offsetHeight)/((body.offsetHeight)))*100}px`;
+
+
+ // console.log(derivedMovement);
+ derivedTransparency = (window.pageYOffset/(body.offsetHeight));
+
+  //top.style.top = `${derivedMovement*400}px`;
+ 
+  if(derivedTransparency>initTransparency){
+    BgVideo.style.opacity = `${1-derivedTransparency}`;
+    if (x.matches) { 
+      VidContainer.style.opacity = `${1-derivedTransparency}`;
+    }
+    else{
+      VidContainer.style.opacity = 1;
+    }
+    console.clear();
+    console.log(BgVideo.style.opacity);
+  }
+  else{
+    BgVideo.style.opacity = `${initTransparency}`;
+    if (x.matches) { 
+      VidContainer.style.opacity = `${initTransparency}`;
+    }
+    else{
+      VidContainer.style.opacity = 1;
+    }
+  }
+  
+}
+
+window.onscroll = function(){
+  scrollfunction();
+};
